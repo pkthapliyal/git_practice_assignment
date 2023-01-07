8cc7d809bf512007cbe86680dbdda9a2c166ea8b
Pankaj Kumar <pkthapliyal101@gmail.com>
2023-01-07 21:20:22 +0530

    First Commit

---
+++ Palindrome.js
@@ -0,0 +1,13 @@
+let str="nrupul" ;
+let N=6 ;
+let bag="";
+for(let i=N-1;i>=0; i--){
+    bag=bag+str[i]
+}
+if(!(str===bag)) {
+  console.log("Yes")    
+  }   
+else{
+    console.log("No")
+}
+}
---
+++ Prime.js
@@ -0,0 +1,15 @@
+//Check 13 is a prime number and Not
+
+let N=13;
+let count=0;
+for(let i=1; i<=13; i++){
+	if(N%i==0){
+		count++ ;
+	}
+}
+if(count>=2){
+	console.log(N,": is Prime Number");
+}
+else{
+	comsole.log(N,": is not a Prime Number")
+}
